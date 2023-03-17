e2b40a5538e42e56712817dcb7165f47f4981fcb
Fahad Ashraf <mfahadbinashraf@gmail.com>
2023-03-18 00:44:51 +0100

    updated docker configs and added first migration

---
+++ backend/database/migrations/2023_03_17_193002_create_short_urls_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('short_urls', function (Blueprint $table) {
+            $table->id();
+            $table->string('long');
+            $table->string('short');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('short_urls');
+    }
+};
